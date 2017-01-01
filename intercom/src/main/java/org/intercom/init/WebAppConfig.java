4ebab07f93d46bcb0842a91fb70c841f3d6a46b4
jayes <jayes@Jayesh-Home-Windows-10>
2017-01-01 14:27:11 +0530

    Added custom login page

--- intercom/src/main/java/org/intercom/init/WebAppConfig.java
+++ intercom/src/main/java/org/intercom/init/WebAppConfig.java
@@ -1,12 +1,16 @@
 package org.intercom.init;
 
 import org.apache.catalina.security.SecurityConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 @Configuration
 @ComponentScan("org.intercom")
@@ -14,5 +18,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableScheduling
 @Import({ SecurityConfig.class })
 public class WebAppConfig extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public UrlBasedViewResolver setupViewResolver() {
+		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		resolver.setOrder(2);
+		resolver.setViewClass(JstlView.class);
+		//LOGGER.debug("View Resolver bean created " + resolver);
+		return resolver;
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations(
+				"/resources/");
+	}
 
 }
